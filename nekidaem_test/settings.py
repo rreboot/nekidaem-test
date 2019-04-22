341c046f56a77f3edf0747df2c0e439c616bcb6a
rreboot <lgeboot@gmail.com>
2019-04-22 14:02:31 +0500

    .....

--- nekidaem_test/settings.py
+++ nekidaem_test/settings.py
@@ -126,6 +126,6 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'lgeboot@gmail.com'
-EMAIL_HOST_PASSWORD = 'ahhfogwokqurgfll'
+EMAIL_HOST_PASSWORD = '' #put your pass
 
 MANAGERS = []
